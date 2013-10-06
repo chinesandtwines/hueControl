d4d87f3407fccb0e7f5f0656f0d3dc5d685dd76c
Nick Camps <nick.camps@ymail.com>
2013-10-06 14:21:09 -0400

    First step of kivy tutorial http://kivy.org/docs/tutorials/pong.html

---
+++ kivy_pong_tutorial.py
@@ -0,0 +1,13 @@
+import kivy
+kivy.require('1.7.2')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+if __name__ == '__main__':
+    MyApp().run()
