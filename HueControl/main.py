6a66f8c5b5c49ff9a30ccd574ee311e4719acbf2
Nick Camps <nick.camps@ymail.com>
2013-10-12 11:58:02 -0400

    Created orkiv directory using: http://archlinux.me/dusty/2013/06/13/creating-an-application-in-kivy-part-1/
    Signed-off-by: Nick Camps <nick.camps@ymail.com>

--- HueControl/main.py
+++ HueControl/main.py
@@ -5,13 +5,14 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 
-# define the Base Class of our Kivy App
-class MyApp(App):
-    # initialize and return Root Widget in build(self)
-    def build(self):
-        # Label will be the Root Widget of this App
-        return Label(text='Hello World')
+#kivy.require('1.7.2')
+
+class PageLayout(Widget):
+    pass
+
+class TestApp(App):
+    PageLayout()
 
 if __name__ == '__main__':
-    MyApp().run()
+    TestApp().run()
 
