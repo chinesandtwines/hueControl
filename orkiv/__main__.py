670c28c6d483258cb7d5b0179479acb3e2f5c8ff
Nick Camps <nick.camps@ymail.com>
2013-10-12 15:24:18 -0400

    Working tabbed window in kivy.
    Almost entirely copied from
    C:\Users\Nick\Downloads\kivy\examples\widgets\tabbedpanel.py
    
    Changed Builder.load_string to Builder.load_file to use an external
    .kv file instead of having it imbedded in the python code as a string.
    
    Signed-off-by: Nick Camps <nick.camps@ymail.com>

--- orkiv/__main__.py
+++ orkiv/__main__.py
@@ -1,8 +1,16 @@
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import ObjectProperty
 
 class AccountDetailsForm(AnchorLayout):
-    pass
+    server_box = ObjectProperty()
+    username_box = ObjectProperty()
+    password_box = ObjectProperty()
+    
+    def login(self):
+        print self.server_box.text
+        print self.username_box.text
+        print self.password_box.text
 
 class Orkiv(App):
     pass
