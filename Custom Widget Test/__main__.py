91525ea4a17561e61a79d8c6b154f85736b7d751
Nick Camps <nick.camps@ymail.com>
2013-10-14 15:05:41 -0400

    Working on clear button for custom Paint Widget defined in .py
    Signed-off-by: Nick Camps <nick.camps@ymail.com>

--- Custom Widget Test/__main__.py
+++ Custom Widget Test/__main__.py
@@ -12,11 +12,11 @@ from kivy.graphics import Color, Ellipse, Line
 Builder.load_file('hueLayout.kv')
 
 class ColorLoopWidget(Widget):
-##    colorloopwidget = ObjectProperty
+    
     def on_touch_down(self, touch):
         with self.canvas:
-##            self.colorloopwidget.canvas.clear()
-            Color(1,1,1,1)
+            self.clear_widgets()
+##            Color(1,1,1,1)
             d = 10
             Ellipse(pos=(touch.x - d/2, touch.y - d/2), size=(d,d))
             touch.ud['line'] = Line(points=(touch.x, touch.y))
@@ -24,42 +24,17 @@ class ColorLoopWidget(Widget):
     def on_touch_move(self, touch):
         touch.ud['line'].points += [touch.x, touch.y]
 
-##class DataPoint(Widget):
-##    def on_touch_down(self, touch):
-##        print touch.x
-##        print touch.y
-##        with self.canvas:
-##            d = 10
-##            Ellipse(pos=(touch.x, touch.y), size=(d,d))
-##            #self.point1.pos = (touch.x-5, touch.x-5)
-##            #return touch.x, touch.y
-        
-
-class HueControl(Widget):
-    point1 = ObjectProperty    
     
-    
-
-    def on_touch_move(self, id):
-        #print id
-        pass
 
 class HueLayout(Widget):
-    cie_chart = ObjectProperty
+    colorloopwidget = ObjectProperty
+    def clear_canvas(self):
+        self.colorloopwidget.canvas.clear()
     
-
-##    point2 = ObjectProperty
-##    point3 = ObjectProperty
-##    bezier1 = ObjectProperty
-    
-        
-    
-        
 
 class HueApp(App):
     def build(self):
         return HueLayout()
-
 
 if __name__ == '__main__':
     HueApp().run()
